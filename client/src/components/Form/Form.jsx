4e62fa62cd596db6bffc561c3b537c6980725f79
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-04-08 08:04:59 -0500

    Adaptando la pagina web responsive

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -8,32 +8,6 @@ const Form = () => {
   const dispatch = useDispatch();
   const types = useSelector((state) => state.types);
 
-  const handleTypes = {
-    1: "normal",
-    2: "fighting",
-    3: "flying",
-    4: "poison",
-    5: "ground",
-    6: "rock",
-    7: "bug",
-    8: "ghost",
-    9: "steel",
-    10: "fire",
-    11: "water",
-    12: "grass",
-    13: "electric",
-    14: "psychic",
-    15: "ice",
-    16: "dragon",
-    17: "dark",
-    18: "fairy",
-    19: "unknown",
-    20: "shadow",
-  };
-
-  const numsTypes = Object.keys(handleTypes);
-  const valuesTypes = Object.values(handleTypes);
-
   const [input, setInput] = useState({
     name: "",
     image: "",
--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,10 +1,5 @@
 const { Sequelize } = require("sequelize");
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DATABASE_NAME,
-} = require("../../config");
+const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_NAME } = process.env;
 
 const database = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE_NAME}`,
