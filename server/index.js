b608571624d7d6dfe56dd8d8572bda6175b686ec
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2024-04-11 15:13:55 -0500

    Development

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const server = require("./src/app");
 const database = require("./src/database/db");
 
 //Development
-// const { PORT } = require("./config");
+const { PORT } = require("./config");
 
 //Production
-const { PORT } = process.env;
+// const { PORT } = process.env;
 
 database.sync().then(() => {
   server.listen(PORT, () => {
--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
 
 //Development
-// const {
-//   DB_USER,
-//   DB_PASSWORD,
-//   DB_HOST,
-//   DATABASE_NAME,
-// } = require("../../config");
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DATABASE_NAME,
+} = require("../../config");
 
 //Production
-const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_NAME } = process.env;
+// const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE_NAME } = process.env;
 
 const database = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE_NAME}`,
